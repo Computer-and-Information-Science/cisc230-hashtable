406f05b590a146098d836caa904892c6be7cc8a2
Norm Lippincott <nl@acm.org>
2024-10-24 04:41:36 -0400

    Added minimal testing code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
+#include "hashtable.h"
 #include <iostream>
 using namespace std;
 
 int main () {
-    cout << "Hello World!\n";
+    // Create a hash table of int's
+    HashTable<int> ht;
+
+    // Add a few entries
+    ht["cat"] = 10;
+    ht["dog"] = 20;
+    ht["bird"] = 30;
+
+    // Add entries that should cause a collision
+    ht["act"] = 40;
+    ht["tac"] = 50;
+
+    // Print the table
+    ht.print(cout);
 }
